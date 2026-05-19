8b668ac7112cfc56cd458daec6690be62e2de0be
Luis Sierra <luis.proyecto.civica@outlook.es>
2026-05-19 21:50:32 +0000

    they werent broken :) but now theyre better and have a test

--- dbt/snapshots/master_files_core/snp_ports.sql
+++ dbt/snapshots/master_files_core/snp_ports.sql
@@ -5,6 +5,7 @@
         target_shema='snapshots',
         unique_key=['location_id', 'country_id', 'geoarea_id' ],
         strategy='check',
+        updated_at='_ingested_at',
         check_cols=['row_hash'],   
         hard_deletes='new_record'
     )
